e561ac2e0ebc20d9cb8205f5deacb3b456bba51f
David Armendariz <david.adrian.a.p@gmail.com>
2024-10-11 20:23:28 -0500

    Adds next public stripe key

--- next-checkout/constants.ts
+++ next-checkout/constants.ts
@@ -1,5 +1,4 @@
 export default {
   endpoint: "http://localhost:8000/api/checkout",
-  stripe_key:
-    "pk_test_51Q8A8Z2Ncsx9PZdv4sIgn45djDPs9CP0ojjygXkTQxUabYQ3pKt7LHrjFz2m1hWGw1bD8gt2b1Y1qrWUsfxioJU800G3k8a9aY",
+  stripe_key: process.env.NEXT_PUBLIC_STRIPE_KEY,
 };

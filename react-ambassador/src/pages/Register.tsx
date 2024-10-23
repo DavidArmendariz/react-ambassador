10388cc2fa8d7c2c6bac6a259848497db080d96c
David Armendariz <david.adrian.a.p@gmail.com>
2024-10-23 13:10:42 -0500

    Fixes firebase config

--- react-ambassador/src/pages/Register.tsx
+++ react-ambassador/src/pages/Register.tsx
@@ -1,116 +1,132 @@
-import React, {Component, SyntheticEvent} from 'react';
-import axios from 'axios';
-import {Redirect} from 'react-router-dom';
-import app from '../firebase';
-import 'firebase/auth';
+import { Component, SyntheticEvent } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import app from "../../firebase";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 class Register extends Component {
-    firstName = '';
-    lastName = '';
-    email = '';
-    password = '';
-    passwordConfirm = '';
-    state = {
-        redirect: false
-    };
+  firstName = "";
+  lastName = "";
+  email = "";
+  password = "";
+  passwordConfirm = "";
+  state = {
+    redirect: false,
+  };
 
-    submit = async (e: SyntheticEvent) => {
-        e.preventDefault();
+  submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
 
-        await axios.post('register', {
-            first_name: this.firstName,
-            last_name: this.lastName,
-            email: this.email,
-            password: this.password,
-            password_confirm: this.passwordConfirm
+    await axios.post("register", {
+      first_name: this.firstName,
+      last_name: this.lastName,
+      email: this.email,
+      password: this.password,
+      password_confirm: this.passwordConfirm,
+    });
+
+    this.setState({
+      redirect: true,
+    });
+  };
+
+  registerExternal = async () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      if (user) {
+        await axios.post("/register/extern", {
+          first_name: user.displayName?.split(" ")[0],
+          last_name: user.displayName?.split(" ")[1],
+          email: user.email,
+          idToken: user.uid,
         });
 
-        this.setState({
-            redirect: true
-        });
+        this.setState({ redirect: true });
+      }
+    } catch (error) {
+      this.setState({
+        error: "Extern authentication failed. Please try again.",
+      });
+    }
+  };
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to={"/login"} />;
     }
 
-    registerExternal = async () => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        const auth = getAuth(app);
+    return (
+      <main className="form-signin">
+        <form onSubmit={this.submit}>
+          <h1 className="h3 mb-3 fw-normal">Please register</h1>
 
-        try {
-            const result = await signInWithPopup(auth, provider);
-            const user = result.user;
+          <div className="form-floating">
+            <input
+              className="form-control"
+              placeholder="First Name"
+              onChange={(e) => (this.firstName = e.target.value)}
+            />
+            <label>First Name</label>
+          </div>
 
-            if (user) {
-                await axios.post('/register/extern', {
-                    first_name: user.displayName?.split(' ')[0],
-                    last_name: user.displayName?.split(' ')[1],
-                    email: user.email,
-                    idToken: user.uid,
-                });
+          <div className="form-floating">
+            <input
+              className="form-control"
+              placeholder="Last Name"
+              onChange={(e) => (this.lastName = e.target.value)}
+            />
+            <label>Last Name</label>
+          </div>
 
-                this.setState({ redirect: true });
-            }
-        } catch (error) {
-            this.setState({ error: 'Extern authentication failed. Please try again.' });
-        }
-    }
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="name@example.com"
+              onChange={(e) => (this.email = e.target.value)}
+            />
+            <label>Email address</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              onChange={(e) => (this.password = e.target.value)}
+            />
+            <label>Password</label>
+          </div>
 
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to={'/login'}/>;
-        }
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password Confirm"
+              onChange={(e) => (this.passwordConfirm = e.target.value)}
+            />
+            <label>Password Confirm</label>
+          </div>
 
-        return (
-            <main className="form-signin">
-                <form onSubmit={this.submit}>
-                    <h1 className="h3 mb-3 fw-normal">Please register</h1>
-
-                    <div className="form-floating">
-                        <input className="form-control" placeholder="First Name"
-                               onChange={e => this.firstName = e.target.value}
-                        />
-                        <label>First Name</label>
-                    </div>
-
-                    <div className="form-floating">
-                        <input className="form-control" placeholder="Last Name"
-                               onChange={e => this.lastName = e.target.value}
-                        />
-                        <label>Last Name</label>
-                    </div>
-
-                    <div className="form-floating">
-                        <input type="email" className="form-control" placeholder="name@example.com"
-                               onChange={e => this.email = e.target.value}
-                        />
-                        <label>Email address</label>
-                    </div>
-
-                    <div className="form-floating">
-                        <input type="password" className="form-control" placeholder="Password"
-                               onChange={e => this.password = e.target.value}
-                        />
-                        <label>Password</label>
-                    </div>
-
-                    <div className="form-floating">
-                        <input type="password" className="form-control" placeholder="Password Confirm"
-                               onChange={e => this.passwordConfirm = e.target.value}
-                        />
-                        <label>Password Confirm</label>
-                    </div>
-
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
-                    <hr>
-                    <button 
-                        type="button" 
-                        className="w-100 btn btn-lg btn-secondary mt-2" 
-                        onClick={this.registerExternal}
-                    >
-                        Register with Google
-                    </button>
-                </form>
-            </main>
-        );
-    }
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Submit
+          </button>
+          <hr />
+          <button
+            type="button"
+            className="w-100 btn btn-lg btn-secondary mt-2"
+            onClick={this.registerExternal}
+          >
+            Regístrate con Google
+          </button>
+        </form>
+      </main>
+    );
+  }
 }
 
 export default Register;

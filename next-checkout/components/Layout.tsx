1364e704d46d5b84e355035efb5e0d9dfe90f441
David Armendariz <david.adrian.a.p@gmail.com>
2024-10-24 01:02:28 -0500

    adds stripejs

--- next-checkout/components/Layout.tsx
+++ next-checkout/components/Layout.tsx
@@ -11,9 +11,7 @@ const Layout = (props) => {
           integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
           crossOrigin="anonymous"
         />
-        <script src="https://js.stripe.com/v3/"></script>
       </Head>
-
       <div className="container">{props.children}</div>
     </>
   );
